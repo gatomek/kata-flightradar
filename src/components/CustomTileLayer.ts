2a97ed3aa709c81e3af01dc70b21aba980321cad
Gatomek <37158592+gatomek@users.noreply.github.com>
2026-07-04 17:42:44 +0200

    Prettier (#3)

--- src/components/CustomTileLayer.ts
+++ src/components/CustomTileLayer.ts
@@ -1,4 +1,4 @@
 export type CustomTileLayer = {
     url: string;
     attribution: string;
-}
+};
--- src/geojson/aircraft.ts
+++ src/geojson/aircraft.ts
@@ -1,23 +1,19 @@
 import type {Feature, FeatureCollection, MultiPoint} from 'geojson';
 
-const aircraft_48af07: number[] = [20.224085, 51.998855];
+const aircraft_48af07: [number, number] = [20.224085, 51.998855];
 
 const aircraftMultiPoint: Feature<MultiPoint> = {
-    type: "Feature",
+    type: 'Feature',
     geometry: {
-        type: "MultiPoint",
-        coordinates: [
-            aircraft_48af07
-        ]
+        type: 'MultiPoint',
+        coordinates: [aircraft_48af07]
     },
     properties: {
-        desc: "Boeing 737 MAX 8"
+        desc: 'Boeing 737 MAX 8'
     }
-}
+};
 
 export const aircraftCollection: FeatureCollection = {
-    type: "FeatureCollection",
-    features: [
-        aircraftMultiPoint
-    ]
-}
+    type: 'FeatureCollection',
+    features: [aircraftMultiPoint]
+};
